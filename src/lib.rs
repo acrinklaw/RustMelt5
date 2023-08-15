970867092914ef98fbefe50054de2f25f6db4f09
Daniel Marrama <danielmarrama@gmail.com>
2023-08-14 23:50:55 -0700

    create commons.rs and methods.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,8 @@
-pub enum NucleotidePair {
-    AA, AC, AG, AU, UU, UG, UC, UA, CC, CA, CG, CU, GA, GG, GC, GU
-}
+pub mod common;
+mod methods;
+use crate::common::{NucleotidePair, Method};
+use crate::methods::NearestNeighbor;
 
-pub struct ThermoProperties {
-    enthalpy: f64,
-    entropy: f64,
-}
-
-impl ThermoProperties {
-    fn from_pair(pair: &NucleotidePair) -> ThermoProperties {
-        match pair {
-            NucleotidePair::AA => ThermoProperties { enthalpy: -7480.0, entropy: -22.3 },
-            NucleotidePair::AC => ThermoProperties { enthalpy: -6320.0, entropy: -15.2 },
-            NucleotidePair::AG => ThermoProperties { enthalpy: -13940.0, entropy: -39.1 },
-            NucleotidePair::AU => ThermoProperties { enthalpy: -6330.0, entropy: -17.7 },
-            NucleotidePair::UU => ThermoProperties { enthalpy: -5430.0, entropy: -14.5 },
-            NucleotidePair::UG => ThermoProperties { enthalpy: -12140.0, entropy: -32.9 },
-            NucleotidePair::UC => ThermoProperties { enthalpy: -9650.0, entropy: -25.0 },
-            NucleotidePair::UA => ThermoProperties { enthalpy: -6470.0, entropy: -17.0 },
-            NucleotidePair::CC => ThermoProperties { enthalpy: -8880.0, entropy: -19.7 },
-            NucleotidePair::CA => ThermoProperties { enthalpy: -5210.0, entropy: -10.7 },
-            NucleotidePair::CG => ThermoProperties { enthalpy: -9470.0, entropy: -23.0 },
-            NucleotidePair::CU => ThermoProperties { enthalpy: -9590.0, entropy: -23.89 },
-            NucleotidePair::GA => ThermoProperties { enthalpy: -5770.0, entropy: -11.9 },
-            NucleotidePair::GG => ThermoProperties { enthalpy: -9660.0, entropy: -22.1 },
-            NucleotidePair::GC => ThermoProperties { enthalpy: -11900.0, entropy: -26.3 },
-            NucleotidePair::GU => ThermoProperties { enthalpy: -6620.0, entropy: -14.3 },
-        }
-    }
-}
 
 pub fn nucleotide_pair_from_str(pair: &str) -> NucleotidePair {
     match pair {
@@ -95,11 +69,16 @@ pub fn compute_thermodynamics(sequence: &str, nucleotide_concentration: f64, na_
     //initial values for turner2006 mrna/rna
     let mut enthalpy: f64 = 0.0;
     let mut entropy: f64 = 0.0;
+
+    // TODO: replace NearestNeighbor with method of choice
+    let method = NearestNeighbor;
     for i in 0..sequence.len() - 1 {
         let pair_str = &sequence[i..i+2];
         let pair = nucleotide_pair_from_str(pair_str);
-        enthalpy += ThermoProperties::from_pair(&pair).enthalpy;
-        entropy += ThermoProperties::from_pair(&pair).entropy;
+
+        let thermo_props = method.from_pair(&pair);
+        enthalpy += thermo_props.enthalpy;
+        entropy += thermo_props.entropy;
     }
     entropy = compute_entropy_1mna(entropy, 0, sequence.len() as i32 - 1);
     let entropy_correction: f64 = -3.22 * ((sequence.len() as f64 - 1.0) * calculate_delta_s(na_concentration, sequence.len()));
